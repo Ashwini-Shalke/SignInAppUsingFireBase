3712101bc09739ae90d8c572bbdd9411861756ba
Ashwini shalke <ashwinishalke050@gmail.com>
2021-05-03 15:58:22 +0530

    [Ashwini] adds gitignore file

---
+++ SignInAppUsingFireBase/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SignInAppUsingFireBase
+//
+//  Created by Ashwini shalke on 03/05/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
